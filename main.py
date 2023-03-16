c0a55f6fd4c1551429de45c3f17aa212c4883a90
Lou1108 <72265570+Lou1108@users.noreply.github.com>
2023-03-16 22:24:14 +0100

    filter for song preferences, and added structure

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import os
 from deepface import DeepFace
 import cv2
 import time
 import get_songs
+import user_recognition
 
-moods= {'Sad' : 0, 'Calm' : 1, 'Energetic' : 2, 'Happy' : 3}
+# TODO check tempo
+# TODO check user identification
+
+moods = {'Sad': 0, 'Calm': 1, 'Energetic': 2, 'Happy': 3}
 
 # Ele's client ID
 client_id = '6e1a09c940a943da95144c6f49a0717b'
@@ -11,23 +16,35 @@ client_secret = 'ccc2af43075641f9899eaaac5b716b8b'
 
 sp = get_songs.authenticate(client_id, client_secret)
 
-
 current_dominant_emotion = []
 current_emotion = []
-# TODO (LOU): store the tempo of current songs 
-tempo_current_song = []
-# TODO: How to store the preferences everyone has 3 preferences 
-#mischasPreference = [rock,ambient,]
+current_song = None
 
+preferences = {'Mischa': ['rock', 'pop', 'classical'],
+               'Elena': ['party', 'classical', 'punk'],
+               'Meli': ['techno', 'latin', 'jazz'],
+               'Lena': ['alternative', 'rap', 'pop']}
 
-# TODO (LOU): Identify person 
+cam_img_name = "/database/current_user.jpg"
+img_paths = user_recognition.get_all_image_paths()
+user_name = 'Mischa'    # todo change to ''
+
 cam = cv2.VideoCapture(0)
 time.sleep(3)
 _, image = cam.read()
 cam.release()
-# for image in database:
-#    DeepFace.verify(database_image, image)
-    
+
+# todo
+user_recognition.save_img(image, cam_img_name)
+# todo
+#for img in img_paths:
+   # r = DeepFace.find(img_path = "current_user.jpg", db_path = os.getcwd()+"/database")
+   # print(r)
+   # todo result = DeepFace.verify(img1_path=cam_img_name, img2_path=img)
+
+  # todo  if result :     # check if this is boolean
+  #      user_name = (result.split("."))[0]
+
 
 while True:
     cam = cv2.VideoCapture(0)
@@ -35,9 +52,8 @@ while True:
     time.sleep(3)
     _, image = cam.read()
     cam.release()
-    #cv2.imwrite('test.png',image)
     data = DeepFace.analyze(image)
-    #print(data[0].keys())
+
     # --> dict_keys(['emotion', 'dominant_emotion', 'region', 'age', 'gender', 'dominant_gender', 'race', 'dominant_race'])
     print(data[0].get('dominant_emotion'))
     # TODO (MISCHA): refactor the current emotion to our available list --> Sad' : 0, 'Calm' : 1, 'Energetic' : 2, 'Happy' : 3
@@ -46,29 +62,28 @@ while True:
 
     # TODO (MELI): Check time if time is ready do calculations below (IF not) otherwise collect emotions (go up to the code 'continue')
 
-
     # TODO (MELI): get average of current_dominant_emotion and clear the old list
 
-    # get songs based on preference 
-    songs = get_songs(sp, 100, get_songs.get_playlist_URI(sp, "genre: "+preference1))
-    songs.append(get_songs(sp, 100, get_songs.get_playlist_URI(sp, "genre: "+preference2)))
-    songs.append(get_songs(sp, 100, get_songs.get_playlist_URI(sp, "genre: "+preference3)))
+    songs = []
+    # get songs based on preference
+    for p in preferences.get(user_name):
+        songs.extend(get_songs.get_songs(sp, 100, get_songs.get_playlist_URI(sp, p)))
+
     data = get_songs.create_table_songs(sp, songs)
 
-    
     # TODO (MISCHA): Classify on emotion with model (NN or RF) taking into account the current dominant emotion
     # TODO (MISCHA): Play around with NN 
     # returns a df of songs matching the two possible emotion
-
 
     # TODO (ELENA): Filter/Order on sad or calm = speechiness --> high speechiness = sad, energetic and happy = loudness --> high loudness = energetic
     # IF time: filter songs based on realeased date matching with the age of the user
     # returns data in a df of songs matching emotion with a filter of < 0.5 based on the emotion
 
-    # TODO (LOOU): Tempo: create a new column (absolut_tempo_error) where the previous tempo .... you will figure it out
-    # returns one song
+    song_rec = get_songs.filter_closest_tempo(data, current_song)
+    current_song = song_rec
+    cur_song_name = song_rec.loc['name']
+    print(cur_song_name)
 
     # TODO (MELI / ALL): Play song 
-    
-    
-    
+
+
